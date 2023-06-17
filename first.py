e7a3c24472f4473eee8d9a039ac4db9e905cc6d9
seljaammu <selja.ksf@gmail.com>
2023-06-17 07:43:35 +0530

    added second excecution

--- first.py
+++ first.py
@@ -5,3 +5,12 @@ x = int(input('enter the first no:'))
 y = int(input('enter the second no:'))
 sum = add(x,y)
 print(sum)
+
+
+def multi(x,y):
+    z = x+y
+    return z
+x = int(input('enter the first no:'))
+y = int(input('enter the second no:'))
+product = multi(x,y)
+print(product)
