ea07e02eeb94a2bc1d7158b61daaf04e9e3a5897
seljaammu <selja.ksf@gmail.com>
2023-06-28 20:01:51 +0530

    div fn added

--- first.py
+++ first.py
@@ -14,3 +14,11 @@ x = int(input('enter the first no:'))
 y = int(input('enter the second no:'))
 product = multi(x,y)
 print(product)
+
+def div(x,y):
+    z = x/y
+    return z
+x = int(input('enter the first no:'))
+y = int(input('enter the second no:'))
+product = div(x,y)
+print(product)
